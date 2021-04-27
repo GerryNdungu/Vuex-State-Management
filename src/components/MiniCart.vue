9eaa9a861f4478b9d1835927d67d949ff5be846e
GerryNdungu <gerryjerry36@gmail.com>
2021-04-27 20:31:12 +0300

    Vuex 1st

---
+++ src/components/MiniCart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dropdown-menu p-2 cart" aria-labelledby="triggerId">
+        <div>
+            <div class="px-2 d-flex justify-content-between" >
+                <div>    
+                    <strong>Product Title</strong>
+                    <br />1 * $23
+                </div>
+                <div>
+                     <a href="#" class="badge badge-secondary">remove</a>
+                </div>
+            </div>
+         </div>
+        <hr>   
+          <div class="d-flex justify-content-between">
+              <span>Total: $23</span>
+              <a href="#">Clear Cart</a>
+          </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.cart{
+     min-width: 320px;
+     right: 0;
+     left: auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Product from '../views/Product.vue'
 const routes = [
   {
     path: '/',
@@ -8,12 +8,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/product/:id',
+    name: 'product',
+    component: Product,
+    props: true
   }
 ]
 
